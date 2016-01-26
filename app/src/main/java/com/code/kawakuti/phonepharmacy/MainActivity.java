76b92c0b99c41504344d4c7894ee697d2203e88e
kawakuticode <kawakuticode@gmail.com>
2016-01-26 13:56:50 +0100

    Database and CRUD Operations Created Init CRUD Tests

--- app/src/main/java/com/code/kawakuti/phonepharmacy/MainActivity.java
+++ app/src/main/java/com/code/kawakuti/phonepharmacy/MainActivity.java
@@ -1,15 +1,26 @@
 package com.code.kawakuti.phonepharmacy;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    List<Med> listMeds = new ArrayList<Med>();
+    DataBaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +29,48 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        db = new DataBaseHandler(getApplicationContext());
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Toast.makeText(MainActivity.this, "Add a new medicine", Toast.LENGTH_SHORT).show();
-                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
+                addMedDialog().show();
             }
         });
     }
+
+
+    public Dialog addMedDialog () {
+         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        // Get the layout inflater
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_addmed, null));
+        builder.setMessage(R.string.dialog_add)
+                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                       dialog.cancel();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+
+    }
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
